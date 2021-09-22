1d4d6fcf0310c5f23a8a7b1ad80b44df4a495622
Cracker <85168111+cracker911181@users.noreply.github.com>
2021-09-22 23:15:32 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ version='2.0',
 description='This PIP only for Encoding Cracker File',
 author='cracker911181',
 url='https://github.com/cracker911181/cracker_enc',
-packages=['cracker_encoder'],
+packages=['cracker_enc'],
 
 zip_safe=False)
